a7e5de5705959ceb71a30448c72b9c2f7bd4a0f3
Adam Johnson <ajohnson@examsoft.com>
2020-06-13 00:52:06 +0100

    https support

--- src/api.js
+++ src/api.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
 import express, { query } from 'express';
 import bodyParser from 'body-parser';
 
@@ -6,7 +9,8 @@ import { dbPromise } from './db.js';
 (async () => {
     const db = await dbPromise;
 
-    const PORT = 3000;
+    const httpPort = 3000;
+    const httpsPort = 3000;
 
     const app = express();
 
@@ -42,9 +46,11 @@ import { dbPromise } from './db.js';
                 projectionObj = { slug: 1, _id: 0 };
             }
 
-            console.log(projectionObj);
-
-            const musicClips = await clipsCollection.find({ channel, $or: [{ song: { $exists: true } }, { fingerprintFailed: true }] }).project(projectionObj).sort({ views: -1 }).toArray();
+            const musicClips = await clipsCollection
+                .find({ channel, $or: [{ song: { $exists: true } }, { fingerprintFailed: true }] })
+                .project(projectionObj)
+                .sort({ views: -1 })
+                .toArray();
 
             return res.send({
                 success: true,
@@ -58,9 +64,28 @@ import { dbPromise } from './db.js';
         }
     });
 
-    app.listen(PORT, () => {
-        console.log(`TwitchMusicID listening on port ${PORT}!`);
-    });
+    if (process.platform === 'win32') {
+        app.listen(httpPort, () => {
+            console.log(`TwitchMusicID http server listening on port ${httpPort}!`);
+        });
+    } else {
+        const privateKey = fs.readFileSync('/etc/letsencrypt/live/vaeb.io/privkey.pem', 'utf8');
+        const certificate = fs.readFileSync('/etc/letsencrypt/live/vaeb.io/cert.pem', 'utf8');
+        const ca = fs.readFileSync('/etc/letsencrypt/live/vaeb.io/chain.pem', 'utf8');
+
+        const credentials = { key: privateKey, cert: certificate, ca };
+
+        const httpServer = http.createServer(app);
+        const httpsServer = https.createServer(credentials, app);
+
+        httpServer.listen(httpPort, () => {
+            console.log(`TwitchMusicID http server listening on port ${httpPort}!`);
+        });
+
+        httpsServer.listen(httpsPort, () => {
+            console.log(`TwitchMusicID https listening on port ${httpsPort}!`);
+        });
+    }
 
     console.log('Ran API module');
 })();
